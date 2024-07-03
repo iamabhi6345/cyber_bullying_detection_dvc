1241524fd40500e39b9d5c9e8c9eb43fb07fa339
ABHISHEK KUMAR <iamabhi6345@gmail.com>
2024-07-03 10:15:50 +0530

    function added to initialize DVC

--- abhishek/version_data.py
+++ abhishek/version_data.py
@@ -1,12 +1,12 @@
+from pathlib import Path 
 from abhishek.config_schemas.config_schema import Config
 from abhishek.utils.config_utils import get_config
+from abhishek.utils.data_utils import initialize_dvc
 
 
 @get_config(config_path="../configs", config_name="config")
-def entrypoint(config: Config) -> None:
-    print(config)
-
-
-if __name__ == "__main__":
-    entrypoint()  # type: ignore
+def version_data (config: Config) -> None: 
+    initialize_dvc()
     
+if __name__ == "__main__":
+    version_data() # type: ignore
